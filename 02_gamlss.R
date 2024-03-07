e09bd9c7961d3d61448d759c6a09652f41e03b5c
Sakura Takahashi <82133070+sparkrones@users.noreply.github.com>
2024-03-07 22:04:33 +1100

    Update 02_gamlss.R

--- 02_gamlss.R
+++ 02_gamlss.R
@@ -1,8 +1,21 @@
-source("C:/Users/cherr/Dropbox/VS Code/research/RA_GH/00_func.R") # change path to your own
+source("C:/Users/cherr/Dropbox/VS Code/research/RA_GH/00_func.R")
 
-# 1. import data
-path <- "C:/Users/cherr/Dropbox/VS Code/research/RA_GH/data/shimanto_y120.bin" # change path to your own
-data <- readBin(path, numeric(), size = 4, n = 240, endian = "little")
+
+# 1. argument settings
+option_list <- list(
+  make_option(c("--data"), type = "character", help="File path to read data", metavar="DATA"),
+)
+
+opt_parser <- OptionParser(usage="Usage: %prog --data=DATA", option_list=option_list)
+opt <- parse_args(opt_parser)
+
+if (is.null(opt$data)) {
+  stop("Please specify the file path using --data option.")
+}
+
+
+# 2. import data
+data <- readBin(opt$data, numeric(), size = 4, n = 240, endian = "little")
 
 amax_list <- list()
 for (i in seq(1, length(data), by = 2)) {
@@ -13,39 +26,40 @@ amax <- do.call(rbind.data.frame, amax_list)
 colnames(amax) <- c("year", "outflow")
 
 
-# 2. estimate 100-year flood benchmark by stationary gumbel distribution
+# 3. estimate 100-year flood benchmark by stationary gumbel distribution
 # fitted to first 30 years amax
 df_30 <- head(amax, 30)
 fit <- fevd(outflow, df_30, type = "Gumbel")
 # plot(fit)
 f_1980s <- as.vector(return.level(fit, return.period = 100))
+# print(f_1980s) # 1702.702
 
 # plot stationary gumbel distribution
 # plot(fit, type = "density", main = "Stationary Gumbel Distribution (1980-2009)", lwd = 2)
 # abline(v = f_1980s, col = "red", lty = 2, lwd =3)
 
 
-# 3. GAMLSS fitting (120 years)
+# 4. GAMLSS fitting (120 years)
 model <- gamlss(outflow ~ year, mu.fo = ~ year, sigma.fo = ~ year, family = "GU", data = amax)
 mu <- lpred(model, what = "mu", type = "response")
 sigma <- lpred(model, what = "sigma", type = "response")
 
 # non-exceedance probability of 100-year flood estimated above in each year
 nonx_prob <- pGU(f_1980s, mu = mu, sigma = sigma)
-
 # return period corresponding to non-exceedance prob in each year
 r <- 1 / (1 - nonx_prob)
-
+  
 # 100-year flood magnitudes
 hundred_f <- qGU(0.99, mu = mu, sigma = sigma)
-# print(hundred_f)
+
+df <- data.frame(amax, hundred_f, rp_i, nonx_prob_i)
+
+# nonexceedance probability & RP of each year's outflow
+# nonx_prob_i <- pGU(amax$outflow, mu = mu, sigma = sigma)
+# rp_i <- 1 / (1 - nonx_prob_i)
 
 
-df <- data.frame(amax, hundred_f, r, nonx_prob)
-# write.csv(df, "df.csv")
-
-
-# plot the 100-year flood magnitude with amax
+# 5. plot the 100-year flood magnitude with amax
 plot(df$year, df$outflow, type = "p", pch = 16, col = "blue", xlab = "year", ylab = "annual max outflow [m^3/s]", main = "Nonstationary 100-year Flood Magnitudes (1980-2099)")
 lines(df$year, df$hundred_f, col = "red", lty = 1, lwd = 3)
 
