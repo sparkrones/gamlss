10c74d44a2da5a92588fe1672940108bc4c1adbc
Sakura Takahashi <82133070+sparkrones@users.noreply.github.com>
2024-03-19 20:17:41 +1100

    Update 02_gamlss.R

--- 02_gamlss.R
+++ 02_gamlss.R
@@ -26,20 +26,25 @@ amax <- do.call(rbind.data.frame, amax_list)
 colnames(amax) <- c("year", "outflow")
 
 
-# 3. estimate 100-year flood benchmark by stationary gumbel distribution
+# 3. outlier detection
+fit_otlr <- fevd(outflow, amax, type = "Gumbel")
+outlier <- as.vector(return.level(fit_otlr, return.period = 80000))
+amax <- subset(amax, outflow <= outlier)
+
+
+# 4. estimate 100-year flood benchmark by stationary gumbel distribution
 # fitted to first 30 years amax
 df_30 <- head(amax, 30)
 fit <- fevd(outflow, df_30, type = "Gumbel")
 # plot(fit)
 f_1980s <- as.vector(return.level(fit, return.period = 100))
-# print(f_1980s) # 1702.702
 
 # plot stationary gumbel distribution
 # plot(fit, type = "density", main = "Stationary Gumbel Distribution (1980-2009)", lwd = 2)
 # abline(v = f_1980s, col = "red", lty = 2, lwd =3)
 
 
-# 4. GAMLSS fitting (120 years)
+# 5. GAMLSS fitting (120 years)
 model <- gamlss(outflow ~ year, mu.fo = ~ year, sigma.fo = ~ year, family = "GU", data = amax)
 mu <- lpred(model, what = "mu", type = "response")
 sigma <- lpred(model, what = "sigma", type = "response")
@@ -52,14 +57,10 @@ r <- 1 / (1 - nonx_prob)
 # 100-year flood magnitudes
 hundred_f <- qGU(0.99, mu = mu, sigma = sigma)
 
-df <- data.frame(amax, hundred_f, rp_i, nonx_prob_i)
-
-# nonexceedance probability & RP of each year's outflow
-# nonx_prob_i <- pGU(amax$outflow, mu = mu, sigma = sigma)
-# rp_i <- 1 / (1 - nonx_prob_i)
+df <- data.frame(amax, hundred_f, r, nonx_prob)
 
 
-# 5. plot the 100-year flood magnitude with amax
+# plot the 100-year flood magnitude with amax
 plot(df$year, df$outflow, type = "p", pch = 16, col = "blue", xlab = "year", ylab = "annual max outflow [m^3/s]", main = "Nonstationary 100-year Flood Magnitudes (1980-2099)")
 lines(df$year, df$hundred_f, col = "red", lty = 1, lwd = 3)
 
