50c7f20c284c86ca0756d5d1a07aa610f9103871
Sakura Takahashi <82133070+sparkrones@users.noreply.github.com>
2025-04-01 00:36:15 +1100

    Add files via upload

---
+++ Local/00_install.R
@@ -0,0 +1,13 @@
+# install packages
+# EXECUTE ONLY ONCE!
+
+install.packages("gamlss")
+install.packages("gamlss.dist")
+install.packages("gamlss.data")
+install.packages("extRemes")
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("optparse")
+install.packages("gridExtra")
+install.packages("gt")
+install.packages("grid")
